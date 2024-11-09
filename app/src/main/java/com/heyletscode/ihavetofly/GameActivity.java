747be8fe6e103d95afda508751803b85a86b6538
Cola <cola_kitti@hotmail.com>
2024-11-10 00:59:52 +0700

    Initial commit

---
+++ app/src/main/java/com/heyletscode/ihavetofly/GameActivity.java
@@ -0,0 +1,45 @@
+package com.heyletscode.ihavetofly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class GameActivity extends AppCompatActivity {
+
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MainActivity.activityList.add(this);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+
+        gameView = new GameView(this, point.x, point.y);
+
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MainActivity.activityList.remove(this);
+    }
+}
---
+++ app/src/main/java/com/heyletscode/ihavetofly/MainActivity.java
@@ -0,0 +1,121 @@
+package com.heyletscode.ihavetofly;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static List<Activity> activityList = new ArrayList<>();
+    private boolean isMute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        activityList.add(this);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+
+        TextView highScoreTxt = findViewById(R.id.highScoreTxt);
+
+        final SharedPreferences prefs = getSharedPreferences("game", MODE_PRIVATE);
+        highScoreTxt.setText("HighScore: " + prefs.getInt("highscore", 0));
+
+        isMute = prefs.getBoolean("isMute", false);
+
+        final ImageView volumeCtrl = findViewById(R.id.volumeCtrl);
+
+        if (isMute)
+            volumeCtrl.setImageResource(R.drawable.ic_volume_off_black_24dp);
+        else
+            volumeCtrl.setImageResource(R.drawable.ic_volume_up_black_24dp);
+
+        volumeCtrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                isMute = !isMute;
+                if (isMute)
+                    volumeCtrl.setImageResource(R.drawable.ic_volume_off_black_24dp);
+                else
+                    volumeCtrl.setImageResource(R.drawable.ic_volume_up_black_24dp);
+
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean("isMute", isMute);
+                editor.apply();
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        // super.onBackPressed();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.exit_dialog_custom, null);
+        builder.setView(dialogView);
+
+        // Create the dialog instance
+        final AlertDialog dialog = builder.create();
+
+        // Set up the positive button action
+        Button positiveButton = dialogView.findViewById(R.id.btnPositive);
+        positiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle positive button click
+                // Finish all activities when the back button is pressed in MainActivity
+                for (Activity activity : activityList){
+                    activity.finish();
+                }
+            }
+        });
+
+        // Set up the negative button action
+        Button negativeButton = dialogView.findViewById(R.id.btnNegative);
+        negativeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle negative button click
+                dialog.dismiss(); // Dismiss the dialog
+            }
+        });
+
+        // Show the dialog
+        dialog.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        activityList.remove(this);
+    }
+}
